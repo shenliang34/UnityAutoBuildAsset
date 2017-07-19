3b62889e747d5da164990d982619fc351a90244f
shenliangliang <971935832@qq.com>
2017-07-19 23:33:18 +0800

    *显示隐藏

---
+++ Assets/Editor/TestEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Test))]
+public class TestEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        var t = target as Test;
+        t.isTest = GUILayout.Toggle(t.isTest, "IsTest");
+        if(t.isTest)
+        {
+            t.range = EditorGUILayout.Slider("Range", t.range,1,100);
+        }
+    }
+	
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    
+    [Header("header")]
+    public bool isTest;
+    [Range(0,100)]
+    public float range = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(isTest);
+        Debug.Log(range);
+	}
+}
